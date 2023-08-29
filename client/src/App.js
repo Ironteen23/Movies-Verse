6980f3c4174e3ba9b8388b73eb4c50a25240991e
Ironteen23 <hpmahimkar@gmai.com>
2023-08-29 16:06:29 +0530

    Client added

---
+++ client/src/App.js
@@ -0,0 +1,66 @@
+ import logo from './logo.svg';
+ import api from './api/axiosConfig';
+ import Layout from './components/Layout';
+ import { Route , Routes } from 'react-router-dom';
+ import { useState , useEffect } from 'react';
+import './App.css';
+import Home from './components/home/Home';
+import Slide from './components/slide/Slide';
+import Carousel from 'react-material-ui-carousel';
+import Header from './components/header/Header';
+import Reviews from './components/reviews/Reviews';
+import Trailer from './components/trailer/Trailer';
+
+function App() {
+
+  const[movies , setMovies] = useState();
+  const[movie , setMovie] = useState();
+  const[reviews , setReviews] = useState();
+
+  const getMovies = async()=>{
+
+    try {
+    const res = await api.get("/api/v1/movies/");
+    console.log("res" , res);
+    setMovies(res.data);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getMovieData = async(movieId)=>{
+    try {
+        const res = await api.get(`api/v1/movies/${movieId}`);
+
+        const singleMovie = res.data;
+        setMovie(singleMovie);
+
+        console.log("single reviews " , singleMovie)
+        setReviews(singleMovie.reviewIds);
+
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getMovies();
+  },[])
+
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Home movies={movies}/>}>Home</Route>
+        <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+        <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/trailer/Trailer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import './Trailer.css'
+import ReactPlayer from 'react-player'
+
+
+const Trailer = () => {
+    let params = useParams()
+    let key = params.ytTrailerId;
+
+    console.log("PARAMS IS" , key);
+
+  return (
+    <div className='react-player-container'> 
+    {(key!=null) ? <ReactPlayer controls="true" playing={true} url={`https://www.youtube.com/watch?v=${key}`} width="100%" height="100%" /> : null}
+
+    </div>
+  )
+}
+
+export default Trailer
